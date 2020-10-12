1398ad696c5afa8f2f32714d7917ca9e0f68cf79
Dmitriy Skrebkov <skredmi91@gmail.com>
2020-10-12 16:40:17 +0300

    fixing the code with eslint 2

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
-const express = require("express");
+const express = require('express');
+
 const app = express();
 const PORT = 3000;
-const path = require("path");
-const routes = require("./routes/index.js");
+const path = require('path');
+const routes = require('./routes/index.js');
 
-app.use(express.static(path.join(__dirname, "public")));
-app.use("/", routes);
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/', routes);
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
